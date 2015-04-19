be5e1dc4cc88c8890b1665a0a78aa0e59666924d
Brent Wilkins <brent@SparkFun.com>
2015-04-18 21:09:44 -0600

    Initial commit with blink sketch

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# state_machines
-Playing around with FSMs on Arduino
+# State Machines
+Playing around with **F**inite **S**tate **M**achines on Arduino. Inspired by Marshall's blog post [here](https://github.com/marshalltaylorSFE/state_machines "another state_machines repository").
---
+++ blink_fsm.ino
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////
+// Simple Finite State Machine implementation of       //
+// of Blink.ino inspired by:                           //
+// https://github.com/marshalltaylorSFE/state_machines //
+//                                                     //
+// Written for an Uno or other Arduino with LED on D13 //
+//                                                     //
+// State transition table:                             //
+//                                                     //
+//  | Num | Name    | LED | Time  | Input 0 |          //
+//  |---------------------------------------|          //
+//  | 0   | LED_ON  | 1   | DELAY | LED_OFF |          //
+//  | 1   | LED_OFF | 0   | DELAY | LED_ON  |          //
+//                                                     //
+/////////////////////////////////////////////////////////
+
+const uint8_t LED_PIN = 13;
+const uint16_t DELAY = 1000;
+uint32_t time;    // size of return type of millis()
+
+typedef struct {
+  uint8_t out;
+  uint16_t time;  // Can't delay longer than 65,535 ms
+  uint8_t next[2];
+} const state_t;
+
+enum transitions: uint8_t { LED_ON, LED_OFF };
+
+state_t FSM[2] = {
+  {1, DELAY, {1}},
+  {0, DELAY, {0}} 
+};
+
+uint8_t state = LED_ON;
+uint8_t input = 0;  // Input is always zero since there aren't any
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);  // Initialize LED pin as an output.
+  time = millis();  // Capture rough start time
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  // Start of FSM
+  // Set the LED for the current state
+  digitalWrite(LED_PIN, FSM[state].out);  // Set the LED
+  // If time is up, transition to next state
+  if ( (millis() - time) >= FSM[state].time )
+  {
+    // Would read input here
+    state = FSM[state].next[input];
+    time = millis();  // 'Reset' timer
+  }
+  // End of FSM
+  
+  // Do other stuff here...
+}
